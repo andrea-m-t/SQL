b3b2711c179267a81e0dfc9701ea461e22790fa9
Andrea Moreno <77305666+andrea-m-t@users.noreply.github.com>
2022-09-12 23:00:51 -0300

    Create Delete_Methods.sql

---
+++ Delete_Methods.sql
@@ -0,0 +1,29 @@
+
+/*FOR MYSQL WORKBENCH
+
+DELETE TABLES*/
+
+#1-FROM CURRENT DATABASE
+DROP TABLE Productos;
+
+#2-IF EXISTS
+DROP TABLE IF EXISTS Productos;
+
+#3-MULTIPLES
+DROP TABLE IF EXISTS Productos, Clientes, Facturas;
+
+
+#DELETE INFO
+#1-FROM
+DELETE FROM Productos WHERE Producto_ID = 5000;
+
+DELETE FROM Clientes WHERE Nombre LIKE "%PEDRO%";
+
+DELETE FROM Facturas WHERE Letra = 'B';
+
+#2-ALL 
+TRUNCATE Clientes;
+
+DELETE FROM Clientes
+
+
